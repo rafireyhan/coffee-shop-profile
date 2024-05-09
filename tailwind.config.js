91609bf6033a172b6672859584673d0b77ccbb50
rafireyhan <redan215@gmail.com>
2024-05-09 20:33:58 +0700

    fix(main): fix hamburger not centered

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: "8px",
+    },
     extend: {},
+    screens: {
+      "2xl": "1320px",
+    },
   },
   plugins: [],
   darkMode: 'class',
